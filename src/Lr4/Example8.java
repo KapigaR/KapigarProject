6d3d19e4538e5d063083b74fd0bf5dda13e19da0
KapigaR <igorpaltsev54@gmail.com>
2022-12-26 12:50:41 +0500

    commit

--- src/Lr4/Example8.java
+++ src/Lr4/Example8.java
@@ -6,17 +6,16 @@ public class Example8 {
 
     public static class encrypt {
         public static String getEncryptString(String encryptString, int shift) {
-            char[] ArrayChar = encryptString.toCharArray(); // преобразуем переданную в метод строку в символьный массив
-            long[] ArrayInt = new long[ArrayChar.length]; // создаем массив с типом данных long, размер массива равен размеру символьного массива
-            char[] ArrayCharNew = new char[ArrayChar.length]; // создаем символьный массив в который будем записывать преобразованные символы
-            //в цикле перебираем все символы из которых состоит переданная нами строка и прибавляем к ним число, ключ (сдвиг)
+            char[] ArrayChar = encryptString.toCharArray();
+            long[] ArrayInt = new long[ArrayChar.length];
+            char[] ArrayCharNew = new char[ArrayChar.length];
             for (int i = 0; i< ArrayChar.length; i++)
             {
-                ArrayInt[i] = ArrayChar[i] + shift; // прибавляем к символу с индексом i сдвиг
-                ArrayCharNew[i] = (char)ArrayInt[i]; // преобразовываем число в символ char и записываем на нужное место в новый массив
+                ArrayInt[i] = ArrayChar[i] + shift;
+                ArrayCharNew[i] = (char)ArrayInt[i];
             }
-            encryptString = new String (ArrayCharNew); // преобразовываем новый массив символов в строку
-            return encryptString; // возвращаем строку в метод из которого был вызван текущий метод
+            encryptString = new String (ArrayCharNew);
+            return encryptString;
         }
     }
 
